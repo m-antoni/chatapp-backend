7259ac7a921108889b0e080797ff67b0179ba225
Michael Antoni <michaelantoni.cs@gmail.com>
2021-09-18 02:00:41 +0800

    add moment for datetime

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const server = require('http').createServer(app);
-const colors = require('colors');
 const cors = require('cors');
+const moment = require('moment');
 const { joinUser, getCurrentUser, userDisconnect } = require('./utils/create_users');
 const io = require('socket.io')(server, {
     cors: {
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 //initializing the socket io connection 
 io.on("connection", (socket) => {
-    socket.emit('message','Socket is running...');
+    // socket.emit('message','Socket is running...');
 
     // for a new user joining the room
     socket.on('join_room', ({ username, roomname }) => {
@@ -21,18 +21,20 @@ io.on("connection", (socket) => {
         const c_user = joinUser(socket.id, username, roomname);
         socket.join(c_user.roomname);
 
-        //display a welcome message to the user who have joined a room
+        // display a welcome message to the user who have joined a room
         socket.emit("message", {
             user_id: c_user.id,
             username: c_user.username,
             text: `Welcome ${c_user.username}`,
+            date: moment().format('MM-DD-YYYY HH:mmA')
         });
 
         // display a welcome message to the user who have joined a room  
         socket.broadcast.to(c_user.roomname).emit('message', {
             user_id: c_user.id,
             username: c_user.username,
-            text: `${c_user.username} has joined the chat room`
+            text: `${c_user.username} has joined the chat room`,
+            date: moment().format('MM-DD-YYYY HH:mmA'),
         });
 
         // user sending the message
@@ -40,10 +42,13 @@ io.on("connection", (socket) => {
             // gets the current user and the message sent
             const c_user = getCurrentUser(socket.id);
 
-            io.to(c_user.room).emit('message', {
+            // console.log(text);
+            // console.log(c_user);
+            io.to(c_user.roomname).emit('message', {
                 user_id: c_user.id,
                 username: c_user.username,
-                text: text
+                text: text,
+                date: moment().format('MM-DD-YYYY HH:mmA'),
             });
         });
         
